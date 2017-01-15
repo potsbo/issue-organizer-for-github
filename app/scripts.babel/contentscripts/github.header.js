af13f9d40d8d397f79a7cf866043bbfe322fe885
Shimpei Otsubo <potsbo@gmail.com>
2017-01-16 01:55:22 +0900

    Add contentscripts/github.header.js

---
+++ app/scripts.babel/contentscripts/github.header.js
@@ -0,0 +1,18 @@
+window.addEventListener('load', () => {
+  // Make pages on GitHub an angular app
+  const html = document.querySelector('html')
+  html.setAttribute('ng-app', '')
+  html.setAttribute('ng-csp', '')
+
+  const header = document.querySelector('.header')
+  header.setAttribute('ng-controller', 'GitHubHeaderController')
+
+  const types = ['pulls', 'issues']
+  types.forEach((type) => {
+    const query = `.header-nav .header-nav-item a[href='/${type}']`
+    const item = document.querySelector(query)
+    item.setAttribute('href', `/${type}{{ query.${type} }}`)
+  })
+
+  angular.bootstrap(html, ['GitHubIssueOrganizer'], [])
+})
