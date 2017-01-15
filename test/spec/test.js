29dd787157af584e69322b113481c20f9ff34747
Shimpei Otsubo <potsbo@gmail.com>
2017-01-16 01:36:22 +0900

    eslint --fix test

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,11 +1,11 @@
-(function () {
-  'use strict';
+(function() {
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
 
-      });
-    });
-  });
-})();
+  describe('Give it some context', function() {
+    describe('maybe a bit more context here', function() {
+      it('should run here few assertions', function() {
+
+      })
+    })
+  })
+})()
