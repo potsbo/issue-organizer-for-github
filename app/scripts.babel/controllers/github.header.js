377348c2c3c3d971985d30fd42e908f1f07d18d0
Shimpei Otsubo <potsbo@gmail.com>
2017-01-21 16:24:45 +0900

    Fix hard code on username

--- app/scripts.babel/controllers/github.header.js
+++ app/scripts.babel/controllers/github.header.js
@@ -1,10 +1,12 @@
 const app = angular.module('GitHubIssueOrganizer', [])
 app.controller('GitHubHeaderController', ($scope) => {
   const queryFor = (type) => {
+    const username = document
+      .querySelector('head meta[name=user-login]')
+      .getAttribute('content')
     // TODO: hard code here
-    const user = 'potsbo'
     const org = 'wantedly'
-    const q = `q=is%3Aopen+is%3A${type}+author%3A${user}+org%3A${org}`
+    const q = `q=is%3Aopen+is%3A${type}+author%3A${username}+org%3A${org}`
     const utf8 = 'utf8=✓'
     return `?${utf8}&${q}`
   }
